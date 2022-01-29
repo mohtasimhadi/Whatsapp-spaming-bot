076e933a78b5c8c53ccf0d54a77fca54310e3963
MOHTASIM HADI RAFI <mohtasimhadi@gmail.com>
2022-01-30 02:49:45 +0600

    send texts on loop

---
+++ SendTextsOnLoop.js
@@ -0,0 +1,10 @@
+var message = prompt("Enter your message", "‎");
+var counter = parseInt(prompt("How many Times ?", 10));
+window.InputEvent = window.Event || window.InputEvent;
+var event = new InputEvent("input", { bubbles: true });
+var textbox = document.getElementsByClassName("_13NKt copyable-text selectable-text")[1];
+for (let index = 0; index < counter; index++) {
+  textbox.innerHTML = message;
+  textbox.dispatchEvent(event);
+  document.getElementsByClassName("_4sWnG")[0].click();
+}
